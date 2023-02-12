aa47bf6404c15290dc19c4cff7b0b34735ec1c33
Mishelle <dmishellehill@gmail.com>
2023-02-11 21:03:30 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#age = 22
+
+# message = "Eligible" if age >= 18 else "Not Eligible"
+# print(message)
+
+# high_income = True
+# good_credit = True
+
+# if high_income and good_credit:
+#     print("Eligible")
+
+
+# if __name__ == '__main__':
+#     print("Hello, World!")
+
+
+# user_name = input("What is your name? ")
+# user_age = input ("what is your age? ")
+
+# new_age = 100 - int(user_age)
+# print(new_age)
+
+# current_year = int(2023)
+# hundred_years = int(current_year) + int(new_age)
+# print(f"Good Morning",user_name,"Based on your current age,",user_age,", you will be", new_age, "in the year", hundred_years,". ")
+
+number = int(input("Tell me a number: "))
+print(number)
